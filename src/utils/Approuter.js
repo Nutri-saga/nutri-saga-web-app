ff831b14084bf798d402cdaeb885a20c0ae82a64
vivekgarrg <85150074+vivekgarrg@users.noreply.github.com>
2022-07-20 23:24:54 +0530

    Lazy loading is implemented on the routes

--- src/utils/Approuter.js
+++ src/utils/Approuter.js
@@ -1,41 +1,42 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
-import AddDish from "../Pages/AddDish";
-import Dishes from "../Pages/Dishes";
-import Planner from "../Pages/Planner";
-import ShoppingList from "../Pages/ShoppingList";
 import ProtectedRoutes from "./ProtectedRoutes";
-import Login from '../Pages/Login';
-import UpdateDish from "../Pages/UpdateDish";
-import AdminDashboard from "../Pages/AdminDashboard";
-import UpdateDishByID from "../Pages/UpdateDishByID";
-import Profile from '../Components/Profile';
-import Home from '../Pages/Home';
-import About from "../Pages/About";
-import Calculator from "../Pages/Calculator";
+
+const AddDish = React.lazy(() => import("../Pages/AddDish"));
+const Dishes = React.lazy(() => import("../Pages/Dishes"));
+const Planner = React.lazy(() => import("../Pages/Planner"));
+const ShoppingList = React.lazy(() => import("../Pages/ShoppingList"));
+const Login = React.lazy(() => import("../Pages/Login"));
+const UpdateDish = React.lazy(() => import("../Pages/UpdateDish"));
+const AdminDashboard = React.lazy(() => import("../Pages/AdminDashboard"));
+const UpdateDishByID = React.lazy(() => import("../Pages/UpdateDishByID"));
+const Profile = React.lazy(() => import("../Components/Profile"));
+const Home = React.lazy(() => import("../Pages/Home"));
+const About = React.lazy(() => import("../Pages/About"));
+const Calculator = React.lazy(() => import("../Pages/Calculator"));
 
 function Approuter() {
   return (
-    <Routes>
-      <Route path='/' element={<Home/>}/>
-      <Route path="/dishes" element={<Dishes />} />
-      <Route path="/planner" element={<Planner />} />
-      <Route path="/shoppinglist" element={<ShoppingList />} />
-      <Route path="/login" element={<Login/>}/>
-      <Route path='/about' element={<About/>}/>
-      <Route path='/calculator' element={<Calculator/>}/>
-      <Route path='/*' element={<Login/>}/>
-      
+    <Suspense fallback={<div>Loading...</div>}>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/dishes" element={<Dishes />} />
+        <Route path="/planner" element={<Planner />} />
+        <Route path="/shoppinglist" element={<ShoppingList />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/calculator" element={<Calculator />} />
+        <Route path="/*" element={<Login />} />
 
-      <Route element={<ProtectedRoutes />}>
-      <Route path="/dashboard" element={<AdminDashboard />} />
-        <Route path="/adddish" element={<AddDish />} />
-        <Route path="/updatedish" element={<UpdateDish />} />
-        <Route path="/updatedish/:id" element={<UpdateDishByID/>}/>
-        <Route path='/profile' element={<Profile/>}/>
-      </Route>
-      
-    </Routes>
+        <Route element={<ProtectedRoutes />}>
+          <Route path="/dashboard" element={<AdminDashboard />} />
+          <Route path="/adddish" element={<AddDish />} />
+          <Route path="/updatedish" element={<UpdateDish />} />
+          <Route path="/updatedish/:id" element={<UpdateDishByID />} />
+          <Route path="/profile" element={<Profile />} />
+        </Route>
+      </Routes>
+    </Suspense>
   );
 }
 
