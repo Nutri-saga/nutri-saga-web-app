c1c6d53f350f81b3b4ef308fa74c69f553b4606e
vivekgarrg <85150074+vivekgarrg@users.noreply.github.com>
2022-08-05 19:04:32 +0530

    a new look given to dish card

--- src/Components/DishCard.jsx
+++ src/Components/DishCard.jsx
@@ -34,75 +34,93 @@ export default React.memo(function DishCard({ val }) {
   };
 
   return (
-    <Card
-      className="dish-card"
-      sx={{ width: 240, height: 540, boxShadow: "gray 2px 5px 15px" }}
-    >
-      <CardMedia
-        component="img"
-        width="auto"
-        height="40%"
-        image={val.image_url["url"]}
-        alt={val.name}
-        sx={{ border: "2px solid green", borderRadius: "5px" }}
+    <>
+      <img
+        width="220px"
+        height="220px"
+        style={{
+          borderRadius: "50%",
+          position: "relative",
+          marginBottom: "-140px",
+          marginLeft: "10px",
+        }}
+        src={val.image_url["url"]}
       />
-      <CardContent>
-        <Typography
-          sx={{
-            fontWeight: "600",
-            letterSpacing: "0.05em",
-            height: "100px",
-            color: "#424242",
-          }}
-          align="center"
-          gutterBottom
-          variant="h5"
-          component="div"
-        >
-          {val.name}
-        </Typography>
-        <Typography align="left" gutterBottom component="div">
-          <span style={{ fontWeight: "600" }}>Servings</span>
-          {`: ${val.servings}`}
-        </Typography>
-        <Typography align="left" gutterBottom component="div">
-          <span style={{ fontWeight: "600" }}>Energy</span>
-          {`: ${val.energy}g`}
-        </Typography>
-        <Typography align="left" gutterBottom component="div">
-          <span style={{ fontWeight: "600" }}>Fats</span>
-          {`: ${val.fats}g`}
-        </Typography>
-        <Typography align="left" gutterBottom component="div">
-          <span style={{ fontWeight: "600" }}>Protein</span>
-          {`: ${val.protein}g`}
-        </Typography>
-        <Typography align="left" gutterBottom component="div">
-          <span style={{ fontWeight: "600" }}>Calories</span>
-          {`: ${val.calories}g`}
-        </Typography>
+      <Card
+        className="dish-card"
+        sx={{
+          width: 240,
+          height: 440,
+          boxShadow: "gray 2px 5px 15px",
+          paddingTop: "120px",
+        }}
+      >
+        {/* <CardMedia
+          component="img"
+          width="auto"
+          height="40%"
+          image={val.image_url["url"]}
+          alt={val.name}
+          sx={{ border: "2px solid green", borderRadius: "5px" }}
+        /> */}
+        <CardContent>
+          <Typography
+            sx={{
+              fontWeight: "600",
+              letterSpacing: "0.05em",
+              height: "100px",
+              color: "#424242",
+            }}
+            align="center"
+            gutterBottom
+            variant="h5"
+            component="div"
+          >
+            {val.name}
+          </Typography>
+          <Typography align="left" gutterBottom component="div">
+            <span style={{ fontWeight: "600" }}>Servings</span>
+            {`: ${val.servings}`}
+          </Typography>
+          <Typography align="left" gutterBottom component="div">
+            <span style={{ fontWeight: "600" }}>Energy</span>
+            {`: ${val.energy}g`}
+          </Typography>
+          <Typography align="left" gutterBottom component="div">
+            <span style={{ fontWeight: "600" }}>Fats</span>
+            {`: ${val.fats}g`}
+          </Typography>
+          <Typography align="left" gutterBottom component="div">
+            <span style={{ fontWeight: "600" }}>Protein</span>
+            {`: ${val.protein}g`}
+          </Typography>
+          <Typography align="left" gutterBottom component="div">
+            <span style={{ fontWeight: "600" }}>Calories</span>
+            {`: ${val.calories}g`}
+          </Typography>
 
-        <div style={{ marginTop: "10px" }}>
-          {" "}
-          {val["status"] == "remove" || cart ? (
-            <Button variant="contained" color="error" onClick={handleRemove}>
-              Remove{" "}
-              <CalendarMonthIcon
-                sx={{ marginTop: "-5px", marginLeft: "3px" }}
-                fontSize="small"
-              />
-            </Button>
-          ) : (
-            <Button variant="contained" color="success" onClick={handleAdd}>
-              Add{" "}
-              <CalendarMonthIcon
-                sx={{ marginTop: "-5px", marginLeft: "3px" }}
-                fontSize="small"
-              />
-            </Button>
-          )}
-        </div>
-      </CardContent>
-    </Card>
+          <div style={{ marginTop: "10px" }}>
+            {" "}
+            {val["status"] == "remove" || cart ? (
+              <Button fullWidth sx={{textTransform:"inherit"}} variant="contained" color="error" onClick={handleRemove}>
+                Remove Dish
+                {/* <CalendarMonthIcon
+                  sx={{ marginTop: "-5px", marginLeft: "3px" }}
+                  fontSize="small"
+                /> */}
+              </Button>
+            ) : (
+              <Button fullWidth sx={{textTransform:"inherit"}} variant="contained" color="success" onClick={handleAdd}>
+                Add Dish
+                {/* <CalendarMonthIcon
+                  sx={{ marginTop: "-5px", marginLeft: "3px" }}
+                  fontSize="small"
+                /> */}
+              </Button>
+            )}
+          </div>
+        </CardContent>
+      </Card>
+    </>
   );
 });
--- src/Components/UpdateDishCards.jsx
+++ src/Components/UpdateDishCards.jsx
@@ -1,34 +1,67 @@
 import React, { useContext, useState, useEffect } from "react";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
-import {useNavigate} from 'react-router-dom';
-import { DishContext } from "../Context/DishContext";
+import { useNavigate } from "react-router-dom";
+
 
 export default React.memo(function UpdateDishCards({ val }) {
-
   const navigate = useNavigate();
 
   return (
-    <Card sx={{ width: 220, height:420 ,cursor:"pointer", boxShadow:"gray 2px 5px 15px" }} onClick={()=>navigate(`/updatedish/${val._id}`)}>
-      <CardMedia
+    <div
+      style={{ cursor: "pointer" }}
+      onClick={() => navigate(`/updatedish/${val._id}`)}
+    >
+      <img
+        width="200px"
+        height="200px"
+        style={{
+          borderRadius: "50%",
+          position: "relative",
+          marginBottom: "-140px",
+          marginLeft: "10px",
+        }}
+        src={val.image_url["url"]}
+      />
+      <Card
+        sx={{
+          width: 220,
+          height: 240,
+          cursor: "pointer",
+          boxShadow: "gray 2px 5px 15px",
+          paddingTop: "120px",
+        }}
+      >
+        {/* <CardMedia
         component="img"
         width="auto"
         height="60%"
         image={val.image_url["url"]}
         alt={val.name}
         sx={{border:"2px solid green", borderRadius:"5px", padding:"3px"}}
-      />
-      <CardContent>
-        <Typography sx={{fontWeight:"600", letterSpacing:"0.05em", height:"100px", color:"#424242"}} align="center" gutterBottom variant="h5" component="div">
-          {val.name}
-        </Typography>
-        <Typography align="left" gutterBottom component="div">
-          <span style={{ fontWeight: "600" }}>Servings</span>
-          {`: ${val.servings}`}
-        </Typography>
-      </CardContent>
-    </Card>
+      /> */}
+        <CardContent>
+          <Typography
+            sx={{
+              fontWeight: "600",
+              letterSpacing: "0.05em",
+              height: "100px",
+              color: "#424242",
+            }}
+            align="center"
+            gutterBottom
+            variant="h5"
+            component="div"
+          >
+            {val.name}
+          </Typography>
+          {/* <Typography align="left" gutterBottom component="div">
+            <span style={{ fontWeight: "600" }}>Servings</span>
+            {`: ${val.servings}`}
+          </Typography> */}
+        </CardContent>
+      </Card>
+    </div>
   );
 });
